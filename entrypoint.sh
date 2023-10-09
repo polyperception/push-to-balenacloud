4d769bcfec481cfcda3efb3bb43409221f3c0054
Nicolas Braem <nicolas.braem@gmail.com>
2023-10-09 20:36:47 +0200

    draft flag (#3)
    * make action fail if push fails
    
    * use bash
    
    * add draft option

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/bash -o pipefail
 
-cd ${GITHUB_WORKSPACE} && cd ${APPLICATION_PATH} && echo -e "name: ${APPLICATION_NAME}\ntype: sw.application\nversion: ${VERSION}" > balena.yml && /balena-cli/balena login --token ${API_TOKEN} && /balena-cli/balena push ${APPLICATION_NAME} | grep -v \\[=*\>
+EXTRA_ARGS=""
+
+if [[ "${DRAFT}" == "true" ]]; then
+  EXTRA_ARGS="--draft"
+fi
+
+cd ${GITHUB_WORKSPACE} && cd ${APPLICATION_PATH} && echo -e "name: ${APPLICATION_NAME}\ntype: sw.application\nversion: ${VERSION}" > balena.yml && /balena-cli/balena login --token ${API_TOKEN} && /balena-cli/balena push ${APPLICATION_NAME} ${EXTRA_ARGS} | grep -v \\[=*\>
