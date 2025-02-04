69842cf5e3f54da1336423c736d2cb2390e0c92f
mmuhiedd <marinmuhieddine17@gmail.com>
2025-02-04 12:30:15 +0100

    Use dockerfile as balena push arg

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,8 @@ if [[ "${DRAFT}" == "true" ]]; then
   EXTRA_ARGS="--draft"
 fi
 
-cd ${GITHUB_WORKSPACE} && cd ${APPLICATION_PATH} && echo -e "name: ${APPLICATION_NAME}\ntype: sw.application\nversion: ${VERSION}" > balena.yml && /balena-cli/balena login --token ${API_TOKEN} && BALENA_DOCKERFILE=${BALENA_DOCKERFILE} /balena-cli/balena push ${APPLICATION_NAME} ${EXTRA_ARGS} | grep -v \\[=*\>
+if [[ -n "${BALENA_DOCKERFILE}" ]]; then
+  EXTRA_ARGS="${EXTRA_ARGS} --dockerfile ${BALENA_DOCKERFILE}"
+fi
+
+cd ${GITHUB_WORKSPACE} && cd ${APPLICATION_PATH} && echo -e "name: ${APPLICATION_NAME}\ntype: sw.application\nversion: ${VERSION}" > balena.yml && /balena-cli/balena login --token ${API_TOKEN} && /balena-cli/balena push ${APPLICATION_NAME} ${EXTRA_ARGS} | grep -v \\[=*\>
