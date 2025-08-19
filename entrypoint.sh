9daabdf0b83fa592838e8386298a4c8d3bfda485
mmuhiedd <marinmuhieddine17@gmail.com>
2025-08-19 15:22:10 +0200

    Balena deploy instead of balena pus

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -o pipefail
 
-EXTRA_ARGS=""
+EXTRA_ARGS=" --debug --tag ${VERSION}"
 
 if [[ "${DRAFT}" == "true" ]]; then
-  EXTRA_ARGS="--draft"
+  EXTRA_ARGS="${EXTRA_ARGS} --draft"
 fi
 
-cd ${GITHUB_WORKSPACE} && cd ${APPLICATION_PATH} && echo -e "name: ${APPLICATION_NAME}\ntype: sw.application\nversion: ${VERSION}" > balena.yml && /balena/bin//balena login --token ${API_TOKEN} && /balena/bin/balena push ${APPLICATION_NAME} ${EXTRA_ARGS} | grep -v \\[=*\>
+cd ${GITHUB_WORKSPACE} && cd ${APPLICATION_PATH} \
+  && /balena/bin//balena login --token ${API_TOKEN} \
+  && /balena/bin/balena deploy ${FLEET_NAME} ${DOCKER_IMAGE_NAME} ${EXTRA_ARGS} | grep -v \\[=*\> 
+
