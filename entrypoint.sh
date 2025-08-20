4d6d3d2f870e23d6d54f4afa3f5c729213cb47e2
Marin <72067598+mmuhiedd@users.noreply.github.com>
2025-08-20 09:52:02 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -o pipefail
 
-EXTRA_ARGS=" --debug --release-tag ${VERSION}"
+EXTRA_ARGS=" --debug"
 
 if [[ "${DRAFT}" == "true" ]]; then
   EXTRA_ARGS="${EXTRA_ARGS} --draft"
 fi
 
 cd ${GITHUB_WORKSPACE} && cd ${APPLICATION_PATH} \
+  && echo -e "name: ${FLEET_NAME}\ntype: sw.application\nversion: ${VERSION}" > balena.yml \
   && /balena/bin//balena login --token ${API_TOKEN} \
   && /balena/bin/balena deploy ${FLEET_NAME} ${DOCKER_IMAGE_NAME} ${EXTRA_ARGS} | grep -v \\[=*\> 
 
