7439c983f8ed2e1743cffce5d82f892726a0f946
mmuhiedd <marinmuhieddine17@gmail.com>
2025-08-25 16:40:01 +0200

    Revert "Echo token"
    This reverts commit bfa4684b1917d7100baf9ead1379935ac5be5870.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,6 @@ if [[ "${DRAFT}" == "true" ]]; then
   EXTRA_ARGS="${EXTRA_ARGS} --draft"
 fi
 
-echo ${API_TOKEN}
-
 cd ${GITHUB_WORKSPACE} && cd ${APPLICATION_PATH} \
   && echo -e "name: ${FLEET_NAME}\ntype: sw.application\nversion: ${VERSION}" > balena.yml \
   && /balena/bin//balena login --token ${API_TOKEN} \
